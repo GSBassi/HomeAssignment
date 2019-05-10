fc21f88233c62cac20d03db238fcddd8b8b830b6
Gurinder Singh Bassi <bassi4.gs@gmail.com>
2019-05-10 00:18:26 -0400

    Content input validated

--- src/HomeAssignment.Application/CMServices/DTO/CreateContentInput.cs
+++ src/HomeAssignment.Application/CMServices/DTO/CreateContentInput.cs
@@ -8,9 +8,13 @@ namespace HomeAssignment.CMServices.DTO
     public class CreateContentInput
     {
         //public int Id { get; set; }
+        private const int PageNameLength = 50;
+        private const int PageContentLength = 2000;
         [Required]
+        [StringLength(PageNameLength)]
         public string pageName { get; set; }
         [Required]
+        [StringLength(PageContentLength)]
         public string pageContent { get; set; }
     }
 }
