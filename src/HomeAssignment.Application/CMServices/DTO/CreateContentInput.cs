e0e86c887971cc4011361f69f6663ae07d81206d
Gurinder Singh Bassi <bassi4.gs@gmail.com>
2019-05-09 23:41:44 -0400

    DTO Validations for  Required fields

--- src/HomeAssignment.Application/CMServices/DTO/CreateContentInput.cs
+++ src/HomeAssignment.Application/CMServices/DTO/CreateContentInput.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace HomeAssignment.CMServices.DTO
@@ -7,8 +8,9 @@ namespace HomeAssignment.CMServices.DTO
     public class CreateContentInput
     {
         //public int Id { get; set; }
+        [Required]
         public string pageName { get; set; }
-       
+        [Required]
         public string pageContent { get; set; }
     }
 }
--- src/HomeAssignment.Application/CMServices/DTO/GetAuthorInput.cs
+++ src/HomeAssignment.Application/CMServices/DTO/GetAuthorInput.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace HomeAssignment.CMServices.DTO
 {
   public  class GetAuthorInput
     {
+        [Required]
         public int Id { get; set; }
     }
 }
